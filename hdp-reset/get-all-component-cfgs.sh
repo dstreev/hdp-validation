f94481d5a153e512d923b80edb95fd556cc4ca95
David W. Streever <david@streever.com>
2015-05-06 10:27:37 -0400

    Added Hbase-site

--- hdp-reset/get-all-component-cfgs.sh
+++ hdp-reset/get-all-component-cfgs.sh
@@ -10,6 +10,6 @@ else
     export HDP_ENV=default
 fi
 
-for component in core-site hadoop-env hcat-env hdfs-site hive-env hive-site mapred-site oozie-env oozie-site tez-site webhcat-site yarn-env yarn-site; do
+for component in core-site hadoop-env hbase-site hcat-env hdfs-site hive-env hive-site mapred-site oozie-env oozie-site tez-site webhcat-site yarn-env yarn-site; do
     ./get-component-cfg.sh $component
 done
