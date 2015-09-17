8af1bfa7468cc82333c09d489e9c985e411a52c5
David W. Streever <david@streever.com>
2015-09-17 15:49:24 -0700

    Remove password from output

--- hdp-reset/get-component-cfg.sh
+++ hdp-reset/get-component-cfg.sh
@@ -18,6 +18,6 @@ mkdir -p ${OUTPUT_DEST}/${OUTPUT_DIR}
 CUR_CONFIG="${OUTPUT_DEST}/${OUTPUT_DIR}/${SECTION}.txt"
 
 # Query Ambari for the sections settings and save to a temp file.
-CMD="${AMBARI_CFG_SCRIPT} -u ${AMBARI_USER} -p ${AMBARI_PASSWORD} -port ${AMBARI_PORT} get ${AMBARI_HOST} ${AMBARI_CLUSTER_NAME} ${SECTION} > ${CUR_CONFIG}"
+CMD="${AMBARI_CFG_SCRIPT} -u ${AMBARI_USER} -p ${AMBARI_PASSWORD} -port ${AMBARI_PORT} get ${AMBARI_HOST} ${AMBARI_CLUSTER_NAME} ${SECTION} | sed -e 's/^PASSWORD.*$//g' > ${CUR_CONFIG} "
 echo ${CMD}
 eval "${CMD}"
